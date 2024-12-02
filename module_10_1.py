1e8bda82398609a92827d8b924ff8a4d705d922b
mnikodim <Marinenik@yandex.ru>
2024-12-02 19:41:11 +0300

    Задача "Потоковая запись в файлы"

--- module_10_1.py
+++ module_10_1.py
@@ -8,36 +8,32 @@ def write_words(word_count, file_name):
             time.sleep(0.1)
             line = f'Какое-то слово № {i + 1}\n'
             f.write(line)
-            print(f'Завершилась запись в файл {file_name}') #, threading.current_thread().name)
+            print(f'Завершилась запись в файл {file_name}, поток: {threading.current_thread().name}')
 
 
-thread1 = threading.Thread(target = write_words, args = (100, 'example8.txt'))
+def first_block():
+    write_words(10, 'example1.txt')
+    write_words(30, 'example2.txt')
+    write_words(200, 'example3.txt')
+    write_words(100, 'example4.txt')
+
+
+def second_block():
+    write_words(10, 'example5.txt')
+    write_words(30, 'example6.txt')
+    write_words(200, 'example7.txt')
+    write_words(100, 'example8.txt')
+
+
+thread1 = threading.Thread(target = first_block)
+thread2 = threading.Thread(target = second_block)
 start_time = time.time()
 thread1.start()
+thread2.start()
 
-write_words(10, 'example1.txt')
-write_words(30, 'example2.txt')
-write_words(200, 'example3.txt')
-write_words(100, 'example4.txt')
+thread1.join()
+thread2.join()
+
 end_time = time.time()
 
-thread1.join()
-
 print(f'Завершение выполнения потока за: {end_time-start_time:.5f}')
-
-write_words(10, 'example5.txt')
-write_words(30, 'example6.txt')
-write_words(200, 'example7.txt')
-end_time= time.time()
-
-thread1.join()
-
-print(f'Завершение выполнения потока за: {end_time-start_time:.5f}')
-
-
-
-
-
-
-
-
