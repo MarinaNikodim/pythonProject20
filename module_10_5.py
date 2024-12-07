606532491c6553a2b39d36321a70f05d2866408e
mnikodim <Marinenik@yandex.ru>
2024-12-07 23:29:15 +0300

    Задача "Многопроцессное считывание"_correction

--- module_10_5.py
+++ module_10_5.py
@@ -1,6 +1,6 @@
 import threading
 import time
-import multiprocessing
+from multiprocessing import Pool
 
 
 def read_info(name): #где name - название файла
@@ -30,15 +30,10 @@ file_names = [f'./file {number}.txt' for number in range(1, 5)]
 
 # Многопроцессный
 if __name__ == '__main__':
-
     start_time2 = time.time()
-    for file_name in file_names:
-        process = multiprocessing.Process(target = read_info, args= (file_name, ))
-        threads.append(process)
-        process.start()
-
-        process.join()
-    end_time2= time.time()
+    with Pool() as pool:
+        pool.map(read_info, file_names)
+    end_time2 = time.time()
 
     print(f'{end_time2 - start_time2}')
 
